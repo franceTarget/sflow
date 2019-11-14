ba551a0a0fb3e27f99671e7bc61a59c76246e756
renchunlin <1260930799@qq.com>
2019-11-14 10:42:33 +0800

    first

---
+++ src/main/java/com/ren/serving/flow/FlowAbleApplication.java
@@ -0,0 +1,11 @@
+package com.ren.serving.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowAbleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlowAbleApplication.class, args);
+    }
+}
